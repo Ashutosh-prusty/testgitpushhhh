1f083fe7433bbd93d96093cb477f92b279ae1a42
Ashutosh-prusty <prustyashutosh456@gmail.com>
2023-09-04 22:50:56 +0530

    this is my first code to push to githubbbbbb.

---
+++ prac.py
@@ -0,0 +1,23 @@
+import pymongo
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://prustyashutosh456:atpbulu123@ashutosh456.enbgpeu.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
